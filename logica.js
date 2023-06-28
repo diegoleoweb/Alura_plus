0aa86b785f1b26476a00a91a1ae2fdc73258996c
diegoleoweb <135560971+diegoleoweb@users.noreply.github.com>
2023-06-28 19:07:28 -0300

    inserindo arquivos Alura Plus

---
+++ logica.js
@@ -0,0 +1,24 @@
+var a = 300 //acao
+//declarando função
+function servico(){
+    console.log('mao de obra')//exibção
+    let b = 25
+    var z = a+b
+    console.log(z)
+}
+// Função para calcular data de entrega
+function dataDeEntrega(){
+    let c = 15 //prazo minimo
+    let d = 5 //prazo final
+    c+d //prazo total
+    var x = c+d
+    console.log(x)
+}
+//ação
+servico()
+dataDeEntrega()
+
+/*a.addEventListener('click',diego())
+var =  variavel a qualquer momento
+const = nao altera no codigo
+let =  usado em um escopo*/
